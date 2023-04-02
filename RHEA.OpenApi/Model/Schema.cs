2c81fbf93b71a30928c37fbda4beda61bbc6f1a3
samatrhea <s.gerene@rheagroup.com>
2023-04-02 21:37:31 +0200

    [Add] Schema.Example: obsolote.message

--- RHEA.OpenApi/Model/Schema.cs
+++ RHEA.OpenApi/Model/Schema.cs
@@ -52,7 +52,7 @@ namespace OpenApi.Model
         /// A free-form property to include an example of an instance for this schema. To represent examples that cannot be naturally represented in JSON or YAML,
         /// a string value can be used to contain the example with escaping where necessary.
         /// </summary>
-        [Obsolete]
+        [Obsolete("The example property has been deprecated in favor of the JSON Schema examples keyword. Use of example is discouraged, and later versions of this specification may remove it.")]
         public object Example { get; set; }
     }
 }
